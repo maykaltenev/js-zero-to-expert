e6eb5c96fad90e5c962f41be94d40d0d8618a7fe
maykaltenev <michaeltenev@gmail.com>
2022-04-22 09:11:56 +0200

    02-Fundamentals-44.Object

---
+++ complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,19 @@
+// 44. Object Methods
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYeah: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function (birthYeah) {
+    //     return 2037 - birthYeah;
+    // }
+    calcAge: function (this) {
+        return 2037 - this.birthYeah;
+    }
+}
+
+console.log(jonas.calcAge())
+// console.log(jonas['calcAge'](1991))
