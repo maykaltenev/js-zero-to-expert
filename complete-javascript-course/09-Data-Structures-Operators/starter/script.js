7e4a9349ddcf3f0396262b1e470e5687494190e7
maykaltenev <michaeltenev@gmail.com>
2022-05-02 22:54:09 +0200

    string method practice

--- complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -283,39 +283,84 @@
 // const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
 // console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.split(' ');
-  const namesUpper = [];
-  for (const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
-  console.log(namesUpper.join(' '))
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '))
+// }
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('jonas schmedtman');
+
+// // Padding
+// const message = 'Go to gate 23';
+// console.log(message.padStart(20, '+').padEnd(35, '+'));
+// console.log('Jonas'.padStart(20, '+'));
+
+// const maskCreditCard = function (number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
+
+// console.log(maskCreditCard(45621123412132489312));
+// console.log(maskCreditCard('45621123412132489312'));
+
+// // Repeat
+// const message2 = 'Bad weather...All Departures...Delayed ';
+// console.log(message2.repeat(5))
+
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${'🛩 '.repeat(n)}`);
+// }
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
+
+// Coding Challenge #4
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM(see code below to insert the elements), and conversion will happen when the button is pressed.
+// Test data(pasted to textarea, including spaces):
+// underscore_case
+// first_name
+// Some_Variable
+// calculate_AGE
+// delayed_departure
+// Should produce this output(5 separate console.log outputs): underscoreCase ✅
+// firstName ✅
+// someVariable ✅
+// calculateAge ✅
+// delayedDeparture ✅
+// Hints:
+// § Remember which character defines a new line in the textarea 😉
+// § The solution only needs to work for a variable made out of 2 words, like a_b
+// § Start without worrying about the ✅. Tackle that only after you have the variable
+// name conversion working 😉
+// § This challenge is difficult on purpose, so start watching the solution in case
+// you're stuck. Then pause and continue!
+// Afterwards, test with your own test data! GOOD LUCK 😀
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   console.log(text);
+//   const rows = text.split('\n')
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// })
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split('+')) {
+
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36)
+  console.log(output)
 }
-capitalizeName('jessica ann smith davis');
-capitalizeName('jonas schmedtman');
-
-// Padding
-const message = 'Go to gate 23';
-console.log(message.padStart(20, '+').padEnd(35, '+'));
-console.log('Jonas'.padStart(20, '+'));
-
-const maskCreditCard = function (number) {
-  const str = number + '';
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
-};
-
-console.log(maskCreditCard(45621123412132489312));
-console.log(maskCreditCard('45621123412132489312'));
-
-// Repeat
-const message2 = 'Bad weather...All Departures...Delayed ';
-console.log(message2.repeat(5))
-
-const planesInLine = function (n) {
-  console.log(`There are ${n} planes in line ${'🛩 '.repeat(n)}`);
-}
-planesInLine(5);
-planesInLine(3);
-planesInLine(12);
