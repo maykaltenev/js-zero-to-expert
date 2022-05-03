6153947cbd51f3f043c40aea5b4e852c759f4e27
maykaltenev <michaeltenev@gmail.com>
2022-05-03 22:54:41 +0200

    10-function

--- complete-javascript-course/10-Functions/starter/script.js
+++ complete-javascript-course/10-Functions/starter/script.js
@@ -41,6 +41,20 @@ const add = (a, b) => a + b;
 
 // Pass functions as arguments to OTHER functions
 const great = () => console.log('Hey, Jonas')
-btnClose.addEventListener('click', great);
+// btnClose.addEventListener('click', great);
 
-counter.inc.bind(someOtherObject)
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed by: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+}
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
