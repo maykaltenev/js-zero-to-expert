7a0c5939ef1804b90f282db932448c0e6247cfca
maykaltenev <michaeltenev@gmail.com>
2022-05-05 22:18:29 +0200

    bind method

--- complete-javascript-course/10-Functions/starter/script.js
+++ complete-javascript-course/10-Functions/starter/script.js
@@ -70,6 +70,84 @@
 
 // console.log(greet('Hello')('Joans'))
 
-let greetArr = greeting => name => `${greeting} ${name}`
+// let greetArr = greeting => name => `${greeting} ${name}`
 
-console.log(greetArr('Hi')('Joans'))
+// console.log(greetArr('Hi')('Joans'))
+
+// const lufthansa = {
+//     airlane: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+// }
+// The call and apply Methods
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     // book: function() {}
+//     book(flightNum, name) {
+//         console.log(
+//             `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//         );
+//         this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//     },
+// };
+
+// lufthansa.book(239, 'Jonas Schmedtmann');
+// lufthansa.book(635, 'John Smith');
+
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     bookings: [],
+// };
+
+// const book = lufthansa.book;
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings)
+
+// book.call(lufthansa, 239, 'Mary Cooper');
+// console.log(lufthansa);
+
+// const swiss = {
+//     name: 'Swiss Air Lines',
+//     iataCode: 'LX',
+//     bookings: [],
+// }
+
+// book.call(swiss, 583, 'Mary Cooper');
+// console.log(swiss);
+// // not so used
+// const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+// // call and spread out the arguments from array
+// book.call(swiss, ...flightData);
+
+// // bind method
+
+// const bookEW = book.bind(eurowings);
+// bookEW(23, 'Steven Willaims');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Jonas Schmedtmann');
+// bookEW23('Martha COoper');
+
+
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//     console.log(this);
+//     this.planes++;
+//     console.log(this.planes);
+// }
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane)
+
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // addVat = value => value + value * 0.23;
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+
