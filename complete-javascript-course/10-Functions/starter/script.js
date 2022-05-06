3046cc13628b257bd528d658ee88c09909865c70
maykaltenev <michaeltenev@gmail.com>
2022-05-06 11:48:17 +0200

    coding challenge 2

--- complete-javascript-course/10-Functions/starter/script.js
+++ complete-javascript-course/10-Functions/starter/script.js
@@ -257,38 +257,63 @@ GOOD LUCK 😀
 //A closure is the closed-over variable environment of the execution context in which a function was created, even after the execution context is gone
 //A closure gives a function access to its outer scope which preserves the scope chain thorough time
 
-let f;
-const g = function () {
-    const a = 23;
-    f = function () {
-        return (a * 2);
-    }
-}
-const h = function () {
-    const b = 777;
-    f = function () {
-        console.log(b * 2);
-    }
-}
-console.log(g())
-console.log(f())
+// let f;
+// const g = function () {
+//     const a = 23;
+//     f = function () {
+//         return (a * 2);
+//     }
+// }
+// const h = function () {
+//     const b = 777;
+//     f = function () {
+//         console.log(b * 2);
+//     }
+// }
+// console.log(g())
+// console.log(f())
 
-//Re-assigning f function
-console.log(h()) // after reassigning new closure the old closure is released 
-console.log(f())
+// //Re-assigning f function
+// console.log(h()) // after reassigning new closure the old closure is released 
+// console.log(f())
 
 
 // Example 2
-const boardPassengers = function (n, wait) {
-    const perGroup = n / 3;
+// const boardPassengers = function (n, wait) {
+//     const perGroup = n / 3;
 
-    setTimeout(function () { }, 1000)
-    console.log(`Will start boarding in ${wait} seconds`);
-    console.log(`There are 3 groups, each with ${perGroup} passengers`, wait * 1000);
+//     setTimeout(function () { }, 1000)
+//     console.log(`Will start boarding in ${wait} seconds`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`, wait * 1000);
 
-    console.log(`Will start boarding in ${wait} seconds`);
-};
+//     console.log(`Will start boarding in ${wait} seconds`);
+// };
 
-boardPassengers(180, 3);
+// boardPassengers(180, 3);
 
-console.log(`Will start boarding in ${wait} seconds`)
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+ 
+Take the IIFE below and at the end of the function, 
+attach an event listener that changes the color of the selected h1 element ('header')
+ to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you)
+ WHY this worked! Take all the time you need. 
+ Think about WHEN exactly the callback function is executed,
+  and what that means for the variables involved in this example.
+ 
+GOOD LUCK 😀
+*/
+
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    let blue = function () {
+        header.style.color = 'blue'
+    }
+    document.querySelector('body').addEventListener('click', blue)
+    console.dir(blue)
+})();
