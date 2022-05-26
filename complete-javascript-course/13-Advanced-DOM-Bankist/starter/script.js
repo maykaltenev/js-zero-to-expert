094400171b31c989d1aab6840ade72304af6bb5f
maykaltenev <michaeltenev@gmail.com>
2022-05-26 15:39:25 +0200

    tabbed component

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -29,4 +29,24 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-startLogOutTimer.alt 
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+console.log(tabsContent)
+
+// change tabs on click! 
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked)
+  //  Guard clause
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  //Activate tab
+  clicked.classList.add('operations__tab--active')
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
