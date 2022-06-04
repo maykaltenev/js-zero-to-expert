094a8ecdd419025872e49223f618d16f4e8508f7
maykaltenev <michaeltenev@gmail.com>
2022-06-04 16:20:33 +0200

    custum popup

--- complete-javascript-course/15-Mapty/starter/script.js
+++ complete-javascript-course/15-Mapty/starter/script.js
@@ -22,9 +22,21 @@ if (navigator.geolocation) {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent);
+            const { lat, lng } = mapEvent.latlng;
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWith: 150,
+                    minWith: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                }))
+                .setPopupContent('Workout')
+                .openPopup();
+        })
+
     }, function () {
         alert('Could not get your position');
     })
