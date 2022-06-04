666f898f4d6c2b0b37788aba9480a8a6acd52464
maykaltenev <michaeltenev@gmail.com>
2022-06-04 15:56:11 +0200

    connect leafflet

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -242,8 +242,8 @@ window.addEventListener('load', function (e) {
   console.log('Page fully loaded', e)
 })
 
-window.addEventListener('beforeunload', function (e) {
-  e.preventDefault();
-  console.log(e)
-  e.returnValue = ''
-})
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e)
+//   e.returnValue = ''
+// })
