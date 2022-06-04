963d6320f848fd5ad619117eb876880522d9db7c
maykaltenev <michaeltenev@gmail.com>
2022-06-04 11:25:42 +0200

    window prevetn

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -233,3 +233,17 @@ const slider = function () {
   });
 };
 slider()
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree build', e)
+})
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e)
+})
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e)
+  e.returnValue = ''
+})
