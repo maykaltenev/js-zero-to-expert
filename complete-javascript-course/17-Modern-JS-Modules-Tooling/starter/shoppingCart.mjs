745d8ed64c7443fe32426fa8373c59c6d19f7150
maykaltenev <michaeltenev@gmail.com>
2022-06-26 13:06:37 +0200

    top level await

--- complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/shoppingCart.mjs
+++ complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/shoppingCart.mjs
@@ -3,6 +3,11 @@ console.log("Exporting module")
 // const shoppingCart = 10;
 export const cart = [];
 
+// Blocking code
+console.log('Start fetching users');
+await fetch('https://jsonplaceholder.typeicode.com/users');
+console.log('Finish fetching users');
+
 // export const addToCart = function (product, quantity) {
 //     cart.push({ product, quantity });
 //     console.log(`${quantity} ${product} added to cart`)
