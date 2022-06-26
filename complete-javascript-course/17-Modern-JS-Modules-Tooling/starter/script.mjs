6f4e6e986a2e7414e2f68eab8080098841523105
maykaltenev <michaeltenev@gmail.com>
2022-06-26 13:17:23 +0200

    module pattern

--- complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.mjs
+++ complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.mjs
@@ -1,42 +1,69 @@
-// Importing module
-// console.log("Importing module");
-// console.log(addToCart('bread', 5))
-// console.log(price, tq)
-// import { addToCart, price, totalQuantity as tq } from "./shoppingCart.js";
+// // Importing module
+// // console.log("Importing module");
+// // console.log(addToCart('bread', 5))
+// // console.log(price, tq)
+// // import { addToCart, price, totalQuantity as tq } from "./shoppingCart.js";
 
-import * as ShoppingCart from './shoppingCart.mjs'
+// const { CardTravel } = require("@material-ui/icons");
 
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart.totalPrice)
+// import * as ShoppingCart from './shoppingCart.mjs'
 
-// we are not missing usually names when exporting default but its possible to be done
-//  import add, { addToCart, price, totalQuantity as tq } from './shoppingCart.js'
-// add('pasta', 5)
+// // ShoppingCart.addToCart('bread', 5);
+// // console.log(ShoppingCart.totalPrice)
 
-
-// import are not copy of the exports they are like live connections => they point to the same place in memory
-import add, { cart } from './shoppingCart.mjs'
-add('pizza', 2)
-add('coffee', 4)
-add('tea', 3)
+// // we are not missing usually names when exporting default but its possible to be done
+// //  import add, { addToCart, price, totalQuantity as tq } from './shoppingCart.js'
+// // add('pasta', 5)
 
 
-console.log(cart)
+// // import are not copy of the exports they are like live connections => they point to the same place in memory
+// import add, { cart } from './shoppingCart.mjs'
+// add('pizza', 2)
+// add('coffee', 4)
+// add('tea', 3)
 
-// console.log('Starting fetching')
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json()
-// console.log(data);
-// console.log('Something')
 
-const getLastPost = async function () {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await res.json()
-    console.log(data);
-    return { title: data.at(-1).title, text: data.at(-1) }
-}
-const lastPost = getLastPost()
-console.log(lastPost)
+// console.log(cart)
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2)
+// // console.log('Starting fetching')
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json()
+// // console.log(data);
+// // console.log('Something')
+
+// const getLastPost = async function () {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json()
+//     console.log(data);
+//     return { title: data.at(-1).title, text: data.at(-1) }
+// }
+// const lastPost = getLastPost()
+// console.log(lastPost)
+
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2)
+
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart SHIPPING COST IST ${shippingCost}`);
+    };
+
+    const orderStock = function (product, quantity) {
+        console.log(`${quantity} ${product} order from supplier`)
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    }
+})();
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
